114f15d7c1538a878922fcea598a1bf41cba2f8d
Shiraz <24913281+smcclennon@users.noreply.github.com>
2019-11-08 17:35:46 +0000

    Fixed unknown bug?

--- assets/js/ifTouchscreen.js
+++ assets/js/ifTouchscreen.js
@@ -1,7 +1,6 @@
-https://stackoverflow.com/questions/3514784
-https://stackoverflow.com/questions/58770367
+// https://stackoverflow.com/questions/574944
+// https://stackoverflow.com/questions/58770367
 function ifTouchscreen(isTouchscreen, notTouchscreen) {
-
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     isTouchscreen();
   } else if(window.matchMedia("(pointer: coarse)").matches) {
