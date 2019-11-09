41ed70802e2fe881de51811e42fa547ac76c7d5c
Shiraz <24913281+smcclennon@users.noreply.github.com>
2019-11-09 23:29:33 +0000

    Better touch detection

--- assets/js/ifTouchscreen.js
+++ assets/js/ifTouchscreen.js
@@ -1,18 +1,25 @@
 // https://stackoverflow.com/questions/574944
 // https://stackoverflow.com/questions/58770367
 function ifTouchscreen(isTouchscreen, notTouchscreen) {
+  function touchCmds() {
+    isTouchscreen();
+    console.log('Touchscreen device detected');
+  }
+  function nonTouchCmds() {
+    notTouchscreen();
+    console.log('Non-touchscreen device detected');
+  }
   if (
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent
     )
   ) {
-    isTouchscreen();
-    console.log("Touchscreen device detected");
-  } else if (window.matchMedia("(pointer: coarse)").matches) {
-    isTouchScreen();
-    console.log("Touchscreen device detected");
+    touchCmds();
+  } else if (window.matchMedia('(pointer: coarse)').matches) {
+    touchCmds();
+  } else if ('ontouchstart' in window || navigator.msMaxTouchPoints) {
+    touchCmds();
   } else {
-    notTouchscreen();
-    console.log("Non-touchscreen device detected");
+    nonTouchCmds();
   }
 }
