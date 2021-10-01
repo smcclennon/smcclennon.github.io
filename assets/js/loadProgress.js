e5c35de3cba0d26641118427f0e7815715464a72
Shiraz <smcclennon@protonmail.com>
2021-10-01 22:57:46 +0100

    Console log loading duration

--- assets/js/loadProgress.js
+++ assets/js/loadProgress.js
@@ -1,3 +1,5 @@
+var start_load = new Date();
+
 NProgress.configure({
   showSpinner: false,
   parent: '#progressBar'
@@ -10,5 +12,7 @@ console.log('==== Loading content... ====');
 window.onload = function() {
   document.getElementById('all-content').style.display = 'block';
   NProgress.done();
-  console.log('==== Content loading complete ====');
+  var end_load = new Date();
+  var duration = end_load - start_load;
+  console.log('==== Content loading complete in ' + duration + 'ms ====');
 };
